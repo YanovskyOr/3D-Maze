93fbfc70358894543ba846a16110169ca3b8b6e8
YanovskyOr <lilia.misotchenko@gmail.com>
2016-09-09 22:35:02 +0300

    update CLI

--- Maze3D/src/view/CLI.java
+++ Maze3D/src/view/CLI.java
@@ -1,5 +1,24 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class CLI {
 
+	
+	 BufferedReader in;
+	 PrintWriter out;
+	 
+	 public CLI(BufferedReader in,PrintWriter out) {
+		this.in=in;
+		this.out=out;
+	}
+	 
+void start(){
+	
+	while(!in.equals("exit")){
+		
+	}
+	
+}
 }
